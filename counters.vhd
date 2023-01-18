26450b17e21b9e334f5862e2459b50f308065185
Michał Kruszewski <mkru@protonmail.com>
2023-01-18 17:48:06 +0100

    Init commit

---
+++ counters.vhd
@@ -0,0 +1,150 @@
+package counters is
+
+   -- Counter_t is a classic wrap around counter.
+   -- This is a type of counter that is used most often.
+   -- Its range is limited by the range of the integer type.
+   type counter_t is record
+      min : integer;
+      max : integer;
+      val : integer;
+   end record;
+
+   function init(max : integer; init_val : integer := 0; min : integer := 0) return counter_t;
+   -- Init_width initializes a counter based on its width assuming the min value of the counter is 0.   -- Max determines whetehr the init value shall equal max (max = true) or min (max = false).
+   function init_width(width : positive; max : boolean := false) return counter_t;
+   -- Equal returns true if counter c value equals x.
+   function equal(c : counter_t; x : integer) return boolean;
+   -- Is_zero returns true if counter c equals 0.
+   -- It fails if zero is not in the range of the counter.
+   function is_zero(c : counter_t) return boolean;
+   -- Is_min returns true if counter c equals its min value.
+   function is_min(c : counter_t) return boolean;
+   -- Is_max returns true if counter c equals its max value.
+   function is_max(c : counter_t) return boolean;
+   -- Inc returns counter c incremented by i.
+   function inc(c : counter_t; i : natural := 1) return counter_t;
+   -- Inc_if returns counter c incremented by i, if the condition cond is met.
+   -- Otherwise it returns counter c.
+   function inc_if(c : counter_t; cond : boolean; i : natural := 1) return counter_t;
+   -- Rst_min returns counter c reset to its min value.
+   function rst_min(c : counter_t) return counter_t;
+   -- Rst_max returns counter c reset to its max value.
+   function rst_max(c : counter_t) return counter_t;
+   -- Set returns counter c with value set to x.
+   -- It fails if x value is not in range.
+   function set(c : counter_t; x : integer) return counter_t;
+
+end package;
+
+package body counters is
+
+   function init(max : integer; init_val : integer := 0; min : integer := 0) return counter_t is
+      variable c : counter_t;
+   begin
+      if max < min then
+         report "max value " & integer'image(max) & " is less than min value " & integer'image(min)
+            severity failure;
+      end if;
+      if init_val < min then
+         report "init value " & integer'image(init_val) & " is less than min value " & integer'image(min)
+            severity failure;
+      end if;
+      if init_val > max then
+         report "init value " & integer'image(init_val) & " is greater than max value " & integer'image(max)
+            severity failure;
+      end if;
+      c.min := min;
+      c.max := max;
+      c.val := init_val;
+      return c;
+   end function;
+
+   function init_width(width : positive; max : boolean := false) return counter_t is
+      variable c : counter_t;
+   begin
+      c.max := 2 ** width - 1;
+      c.min := 0;
+      if max = false then
+         c.val := 0;
+      else
+         c.val := c.max;
+      end if;
+      return c;
+   end function;
+
+   function equal(c : counter_t; x : integer) return boolean is
+   begin
+      if c.val = x then return true; end if;
+      return false;
+   end function;
+
+   function is_zero(c : counter_t) return boolean is
+   begin
+      if 0 < c.min or 0 > c.max then
+         report "0 is not within counter range <" & integer'image(c.min) & ";" & integer'image(c.max) & ">"
+            severity failure;
+      end if;
+      if c.val = 0 then
+         return true;
+      end if;
+      return false;
+   end function;
+
+   function is_min(c : counter_t) return boolean is
+   begin
+      if c.val = c.min then
+         return true;
+      end if;
+      return false;
+   end function;
+
+   function is_max(c : counter_t) return boolean is
+   begin
+      if c.val = c.max then
+         return true;
+      end if;
+      return false;
+   end function;
+
+   function inc(c : counter_t; i : natural := 1) return counter_t is
+      variable r : counter_t := c;
+      variable rang : positive := r.max - r.min + 1;
+   begin
+      r.val := (r.val + (i mod rang)) mod rang;
+      return r;
+   end function;
+
+   function inc_if(c : counter_t; cond : boolean; i : natural := 1) return counter_t is
+   begin
+      if cond then
+         return inc(c, i);
+      end if;
+      return c;
+   end function;
+
+   function rst_min(c : counter_t) return counter_t is
+      variable r : counter_t := c;
+   begin
+      r.val := r.min;
+      return r;
+   end function;
+
+   function rst_max(c : counter_t) return counter_t is
+      variable r : counter_t := c;
+   begin
+      r.val := r.max;
+      return r;
+   end function;
+
+   function set(c : counter_t; x : integer) return counter_t is
+      variable r : counter_t := c;
+   begin
+      if x < r.min or r.max < x then
+         report "value " & integer'image(x) & " is not within counter range <" & integer'image(c.min) & ";" & integer'image(c.max) & ">"
+            severity failure;
+      end if;
+      r.val := x;
+      return r;
+   end function;
+
+end package body;
---
+++ tb_counter.vhd
@@ -0,0 +1,172 @@
+library work;
+   use work.counters.all;
+
+entity tb_counter is
+end entity;
+
+architecture test of tb_counter is
+begin
+
+   test_init : process is
+      constant MIN : integer := -7;
+      constant MAX : integer := 8917;
+      constant VAL : integer := 147;
+      variable c : counter_t;
+   begin
+      c := init(MAX);
+      assert c.min = 0;
+      assert c.val = 0;
+      assert c.max = MAX;
+      c := init(MAX, VAL);
+      assert c.min = 0;
+      assert c.val = VAL;
+      assert c.max = MAX;
+      c := init(MAX, VAL, MIN);
+      assert c.min = MIN;
+      assert c.val = VAL;
+      assert c.max = MAX;
+      wait;
+   end process;
+
+
+   test_init_width : process is
+      constant WIDTH : integer := 8;
+      variable c : counter_t;
+   begin
+      c := init_width(WIDTH);
+      assert c.min = 0;
+      assert c.val = 0;
+      assert c.max = 255;
+      c := init_width(WIDTH, true);
+      assert c.min = 0;
+      assert c.val = 255;
+      assert c.max = 255;
+      wait;
+   end process;
+
+
+   test_equal : process is
+      constant MAX : integer := 1023;
+      variable c : counter_t := init(MAX);
+   begin
+      assert equal(c, 0);
+      c := inc(c);
+      assert equal(c, 1);
+      c := inc(c, 1000);
+      assert equal(c, 1001);
+      wait;
+   end process;
+
+
+   test_is_zero : process is
+      constant MAX : integer := 1023;
+      variable c : counter_t := init(MAX);
+   begin
+      assert is_zero(c);
+      c := inc(c);
+      assert not is_zero(c);
+      wait;
+   end process;
+
+
+   test_is_min : process is
+      constant MAX : integer := 1023;
+      constant VAL : integer := 1023;
+      constant MIN : integer := 1023;
+      variable c : counter_t := init(MAX, VAL, MIN);
+   begin
+      assert is_min(c);
+      c := inc(c);
+      assert not is_min(c);
+      wait;
+   end process;
+
+
+   test_is_max : process is
+      constant MAX : integer := 1023;
+      variable c : counter_t := init(MAX);
+   begin
+      assert not is_max(c);
+      c := inc(c, MAX);
+      assert is_max(c);
+      wait;
+   end process;
+
+
+   test_inc : process is
+      constant MAX : integer := 15;
+      variable c : counter_t := init(MAX);
+   begin
+      c := inc(c);
+      assert equal(c, 1);
+      c := inc(c, 4);
+      assert equal(c, 5);
+      c := inc(c, 12);
+      assert equal(c, 1);
+      c := inc(c, 32);
+      assert equal(c, 1);
+      c := inc(c, 15);
+      assert equal(c, 0);
+      wait;
+   end process;
+
+
+   test_inc_if : process is
+      constant MAX : integer := 15;
+      variable c : counter_t := init(MAX);
+   begin
+      c := inc_if(c, true, 3);
+      assert equal(c, 3);
+      c := inc_if(c, false);
+      assert equal(c, 3);
+      c := inc_if(c, false, 3);
+      assert equal(c, 3);
+      wait;
+   end process;
+
+
+   test_rst_min : process is
+      constant MAX : integer := 15;
+      constant MIN : integer := 7;
+      variable c : counter_t := init(MAX, MIN, MIN);
+   begin
+      assert is_min(c);
+      c := inc(c);
+      assert not is_min(c);
+      c := rst_min(c);
+      assert is_min(c);
+      wait;
+   end process;
+
+
+   test_rst_max : process is
+      constant MAX : integer := 15;
+      constant MIN : integer := 7;
+      variable c : counter_t := init(MAX, MAX, MIN);
+   begin
+      assert is_max(c);
+      c := inc(c);
+      assert not is_max(c);
+      c := rst_max(c);
+      assert is_max(c);
+      wait;
+   end process;
+
+
+   test_set : process is
+      constant MAX : integer := 15;
+      constant MIN : integer := -15;
+      variable c : counter_t := init(MAX, MIN, MIN);
+   begin
+      c := set(c, 0);
+      assert is_zero(c);
+      c := set(c, MIN);
+      assert is_min(c);
+      c := set(c, MAX);
+      assert is_max(c);
+      c := set(c, 3);
+      assert equal(c, 3);
+      wait;
+   end process;
+
+end architecture;
